37b1f3c1f45aef0eaa9f9e1dc65ee028203f4eec
Sean Corcoran <sean.corcoran@myxplor.com>
2021-09-11 13:01:55 +1000

    first commit

---
+++ src/BannerImage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BannerImage() {
+    return(
+        <div class="banner-image-container">
+            <div class="banner-image"></div>
+        </div>
+    )
+}
+
+export default BannerImage;
---
+++ src/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function NavBar() {
+    return(
+        <div>
+    <ul class="nav-bar">
+            <li><a href="#about">IService</a></li>
+            <li><a href="#news">Post a task</a></li>
+            <li><a href="#contact">Become an expert</a></li>
+            <li><a href="#contact">Find tasks</a></li>
+            <li><a href="#contact">Find tasks</a></li>
+            <li><a href="#contact">Sign in</a></li>
+    </ul>
+  </div>
+    )
+}
+
+export default NavBar;
